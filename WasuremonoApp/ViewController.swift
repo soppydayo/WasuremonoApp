e19c9c7f56d7c7ec3c5c318672ea6a21c3b26f16
soppydayo <souta060614@gmail.com>
2024-01-12 18:14:48 +0900

    Initial Commit

---
+++ WasuremonoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WasuremonoApp
+//
+//  Created by 白川創大 on 2024/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
